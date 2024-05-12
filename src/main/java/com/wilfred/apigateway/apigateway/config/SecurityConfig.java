5a571280880d7229be0991e72971408f4e9743f8
Wilfred <wilfredkim5@gmail.com>
2024-05-12 13:53:46 +0300

    Add Swagger configs to api gateway

--- src/main/java/com/wilfred/apigateway/apigateway/config/SecurityConfig.java
+++ src/main/java/com/wilfred/apigateway/apigateway/config/SecurityConfig.java
@@ -9,10 +9,15 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
+    private String whistedListedUrls[] = {"/swagger-ui.html", "/swagger-ui/**", "/v1/api-docs", "/v1/api-docs/**",
+            "/api-docs/**", "/aggregate/**", "/swagger-resources/**"
+    };
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-       return httpSecurity.authorizeHttpRequests(authorize -> authorize.anyRequest()
-                        .authenticated())
+        return httpSecurity.authorizeHttpRequests(authorize ->
+                        authorize.requestMatchers(whistedListedUrls).permitAll().anyRequest()
+                                .authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults())).build();
 
     }
--- src/main/java/com/wilfred/apigateway/apigateway/routes/Routes.java
+++ src/main/java/com/wilfred/apigateway/apigateway/routes/Routes.java
@@ -9,6 +9,8 @@ import org.springframework.web.servlet.function.RequestPredicates;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
 
+import static org.springframework.cloud.gateway.server.mvc.filter.FilterFunctions.setPath;
+
 @Configuration
 public class Routes {
     @Value("${routes.url.order-service}")
@@ -23,23 +25,36 @@ public class Routes {
     @Bean
     public RouterFunction<ServerResponse> responseProductService() {
 
-        return GatewayRouterFunctions.route("product-service")
-                .route(RequestPredicates.path("/api/v1/product"), HandlerFunctions.http(productServiceEndpoint))
-                .build();
+        return GatewayRouterFunctions.route("product_service").route(RequestPredicates.path("/api/v1/product"), HandlerFunctions.http(productServiceEndpoint)).build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> responseProductServiceSwaggerConfig() {
+
+        return GatewayRouterFunctions.route("product_service_swagger").route(RequestPredicates.path("/aggregate/product-service/v1/api-docs"), HandlerFunctions.http(productServiceEndpoint)).filter(setPath("/api-docs")).build();
     }
 
     @Bean
     public RouterFunction<ServerResponse> responseOrderService() {
-        return GatewayRouterFunctions.route("order-service")
-                .route(RequestPredicates.path("/api/v1/orders"), HandlerFunctions.http(orderServiceEndpoint))
+        return GatewayRouterFunctions.route("order_service").route(RequestPredicates.path("/api/v1/orders"), HandlerFunctions.http(orderServiceEndpoint)).build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> responseOrderServiceSwaggerConfig() {
+        return GatewayRouterFunctions.route("order_service_swagger").route(RequestPredicates.path("/aggregate/order-service/v1/api-docs"), HandlerFunctions.http(orderServiceEndpoint))
+                .filter(setPath("/api-docs"))
                 .build();
     }
 
     @Bean
     public RouterFunction<ServerResponse> responseInventoryService() {
-        return GatewayRouterFunctions.route("inventory-service")
-                .route(RequestPredicates.path("/api/v1/inventories"), HandlerFunctions.http(inventoryServiceEndpoint))
-                .build();
+        return GatewayRouterFunctions.route("inventory_service").route(RequestPredicates.path("/api/v1/inventories"), HandlerFunctions.http(inventoryServiceEndpoint)).build();
     }
 
+    @Bean
+    public RouterFunction<ServerResponse> responseInventoryServiceSwaggerConfig() {
+        return GatewayRouterFunctions.route("inventory_service_swagger").route(RequestPredicates.path("/aggregate/inventory-service/v1/api-docs"), HandlerFunctions.http(inventoryServiceEndpoint))
+                .filter(setPath("/api-docs"))
+                .build();
+    }
 }
